0eab9307885f70e0371f37aedae7d457c4113dc2
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-02-21 11:26:08 +0800

    Update IKin.py

--- Robotics_2/Python_programs/IKin.py
+++ Robotics_2/Python_programs/IKin.py
@@ -1,33 +1,44 @@
+### Inverse Kinematics of SCARA-V3
+
 import numpy as np
 
-#Inverse Kinematics Using Graphical Method
-
-#link lengths in mm
+# link lengths in mm
 a1 = float(input("a1 = "))
 a2 = float(input("a2 = "))
 a3 = float(input("a3 = "))
 a4 = float(input("a4 = "))
 
-#Position Vector in mm
-xe = float(input("X = "))
-ye = float(input("Y = "))
-ze = float(input("Z = "))
+# Position Vector in mm
+x0_3 = float(input("x0_3 = "))
+y0_3 = float(input("y0_3 = "))
+z0_3 = float(input("z0_3 = "))
 
-# To solve for Theta 2 or Th2
-phi2 = np.arctan(ye/xe) #1
-r1 = np.sqrt(ye**2 + xe**2) #2
-phi1 = np.arccos((a4**2 - a2**2 - r1**2)/(-2 * a2 * r1)) #3
-Th2 = (phi2 - phi1) * 180/np.pi #4
+# Inverse Kinematic Solutions using Graphical Method
 
-# To solve for Theta 3 or Th3
-phi3 = np.arccos((r1**2 - a2**2 - a4**2)/(-2 * a2 * a4)) #5
-Th3 = 180 - (phi3*180/np.pi) #6
+# Solution 1
+phi2 = np.arctan(y0_3/x0_3)
+phi2 = phi2*180/np.pi
 
-# To solve for D1
-D1 = ze - a1 - a3 #7
+# Solution 2
+r1 = np.sqrt((y0_3**2)+(x0_3**2))
 
-print("D1 = ", np.around(D1,3))
+# Solution 3
+phi1 = np.arccos((a4**2-r1**2-a2**2)/(-2*r1*a2))
+phi1 = phi1*180/np.pi
 
-print("Th2 = ", np.around(Th2,3))
+# Solution 4
+theta2 = phi2 - phi1
 
-print("Th3 = ", np.around(Th3,3))
+# solution 5
+phi3 = np.arccos((r1**2-a2**2-a4**2)/(-2*a2*a4))
+phi3 = phi3*180/np.pi
+
+# Solution 6
+theta3 = 180 - phi3
+
+# Solution 7
+d1 = z0_3 - a1 - a3
+
+print("d1 = ", np.around(d1,3))
+print("theta2 = ", np.around(theta2,3))
+print("theta3 = ", np.around(theta3,3))
