d42c44422393492395ffefef6202610bb06f3330
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2025-02-10 11:26:16 +0800

    update

--- MexE_409_Robotics_2/MATLAB_sample_programs/2024-2025/Spartankit_3DOF_test.m
+++ MexE_409_Robotics_2/MATLAB_sample_programs/2024-2025/Spartankit_3DOF_test.m
@@ -7,9 +7,9 @@ disp('Spartankit')
 syms a1 a2 a3
 
 %% Link lengths
-a1 = 60;
-a2 = 70;
-a3 = 30;
+a1 = 6;
+a2 = 5;
+a3 = 4;
 
 %% D-H Parameters [theta, d, r, alpha, offset]
 % if prismatic joint: theta = theta, d = 0, offset = 1, after offset put the value of d
@@ -25,7 +25,7 @@ H2_3 = Link([0,0,a3,0,0,0]);
 H2_3.qlim = pi/180*[-90 90];
 
 Spart = SerialLink([H0_1 H1_2 H2_3], 'name', 'Spartankit')
-Spart.plot([0 0 0], 'workspace', [-30 200 -200 200 0 250])
+Spart.plot([0 0 0], 'workspace', [-3 20 -20 20 0 25])
 
 figure(1)
 Spart.teach
